04d12eb35e2e0178b1d28d26a761f694dbe772c2
Sergey Petrov <sergoslav@gmail.com>
2013-02-19 23:25:13 +0400

    fix address export

--- app/code/local/Snowcommerce/ViewQuote/Block/Adminhtml/Sales/Quote/Renderer/Address.php
+++ app/code/local/Snowcommerce/ViewQuote/Block/Adminhtml/Sales/Quote/Renderer/Address.php
@@ -2,8 +2,10 @@
 
 class Snowcommerce_ViewQuote_Block_Adminhtml_Sales_Quote_Renderer_Address extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 {
-	public function render(Varien_Object $quote)
-	{
+    const DELIMETR_LINE = ', ';
+
+    public function render(Varien_Object $quote)
+    {
         /** @var $quote Mage_Sales_Model_Quote */
 //        Mage::log($quote->getShippingAddress()->debug(), null, 'debug_SH.log');
 
@@ -11,17 +13,17 @@ class Snowcommerce_ViewQuote_Block_Adminhtml_Sales_Quote_Renderer_Address extend
         if($quote && $address = $quote->getShippingAddress()){
 //            $html = $address->getStreet(1)."\r\n".$address->getStreet(2)."\r\n".$address->getCity()."\r\n".$address->getRegion()."\r\n".$address->getPostcode()."\r\n".$address->getCountry();
             if ($address->getCompany()) {
-                $html .= $address->getCompany()."\r\n";
+                $html .= $address->getCompany().self::DELIMETR_LINE;
             }
             if ($address->getStreet(1)) {
-                $html .= $address->getStreet(1)."\r\n";
+                $html .= $address->getStreet(1).self::DELIMETR_LINE;
             }
             if ($address->getStreet(2)) {
-                $html .= $address->getStreet(2)."\r\n";
+                $html .= $address->getStreet(2).self::DELIMETR_LINE;
             }
-            $html .= $address->getCity()."\r\n";
-            $html .= $address->getRegion()."\r\n";
-            $html .= $address->getPostcode()."\r\n";
+            $html .= $address->getCity().self::DELIMETR_LINE;
+            $html .= $address->getRegion().self::DELIMETR_LINE;
+            $html .= $address->getPostcode().self::DELIMETR_LINE;
 
             if ($address->getCountry() && $country = Mage::getModel('directory/country')->loadByCode($address->getCountry())){
                 $html .= $country->getName();;
